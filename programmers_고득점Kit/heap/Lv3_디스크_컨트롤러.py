c2ac6377d67832e117be2071aca2065557b61a28
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-11-24 15:01:18 +0900

    add programmers heap level 3

--- programmers_고득점Kit/heap/Lv3_디스크_컨트롤러.py
+++ programmers_고득점Kit/heap/Lv3_디스크_컨트롤러.py
@@ -1,2 +1,30 @@
 #%%
 # https://school.programmers.co.kr/learn/courses/30/lessons/42627
+
+from heapq import *
+
+def solution(jobs):
+    work_count = len(jobs)
+    current_jobs = []
+    heapify(jobs)
+    time = 0
+    answer = 0
+
+
+    while jobs or current_jobs:
+        if jobs and jobs[0][0] <= time:
+            start, work = heappop(jobs)
+            heappush(current_jobs, (work, start))
+        elif current_jobs:
+            work, start = heappop(current_jobs)
+            answer += work + time - start
+            time += work
+        else:
+            start, work = heappop(jobs)
+            answer += work
+            time = start + work
+
+    return answer // work_count
+
+solution([[24, 10], [28, 39], [43, 20], [37, 5], [47, 22], [20, 47], [15, 34], [15, 2], [35, 43], [26, 1]])
+# %%
--- programmers_고득점Kit/heap/Lv3_이중우선순위큐.py
+++ programmers_고득점Kit/heap/Lv3_이중우선순위큐.py
@@ -1,2 +1,45 @@
 #%%
 # https://school.programmers.co.kr/learn/courses/30/lessons/42628
+
+from heapq import *
+
+def solution(operations):
+    max_heap = []
+    min_heap = []
+
+    for operation in operations:
+        oper, value = operation.split()
+        value = int(value)
+
+        if oper == "I":
+            heappush(max_heap, -value)
+            heappush(min_heap, value)
+        else:
+            if max_heap:
+                if value == 1:
+                    max_value = -heappop(max_heap)
+                    min_heap.remove(max_value)
+                    heapify(min_heap)
+                else:
+                    min_value = heappop(min_heap)
+                    max_heap.remove(-min_value)
+                    heapify(max_heap)
+
+    if len(max_heap) >= 2:
+        max = -heappop(max_heap)
+        min_heap.remove(max)
+        heapify(min_heap)
+        min = heappop(min_heap)
+
+        return [max, min]
+    
+    elif len(max_heap) == 1:
+        max = -heappop(max_heap)
+
+        return [max, max]
+    
+    else:
+        return [0, 0]
+
+solution(["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"])
+# %%
