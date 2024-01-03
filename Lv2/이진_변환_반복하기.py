6d32529f203034ef98ac46da63d0b3664bb311a2
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2024-01-04 00:34:17 +0900

    Update 이진_변환_반복하기.py

---
+++ Lv2/이진_변환_반복하기.py
@@ -0,0 +1,16 @@
+#%%
+# https://school.programmers.co.kr/learn/courses/30/lessons/70129
+
+def solution(s):
+    count = 0
+    del_0 = 0
+
+    while s != "1":
+        count += 1
+        num = len(s)
+        s = s.count("1")*"1"
+        del_0 += num - len(s)
+        s = len(s)
+        s = bin(s)[2:]
+
+    return [count,del_0]
