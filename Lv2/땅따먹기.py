179a4c128365d34495ce53bf60986d2e10ef54dc
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2024-03-13 10:39:12 +0900

    Create 땅따먹기.py

---
+++ Lv2/땅따먹기.py
@@ -0,0 +1,8 @@
+#%%
+# https://school.programmers.co.kr/learn/courses/30/lessons/12913
+def solution(land):
+    for i in range(1,len(land)):
+        for j in range(len(land[0])):
+            land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
+
+    return max(land[len(land)-1])
