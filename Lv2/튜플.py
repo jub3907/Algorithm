fa0077ea18db680cf7c4aa99fb78cd60425e05b4
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2024-03-16 18:32:19 +0900

    Create 튜플.py

---
+++ Lv2/튜플.py
@@ -0,0 +1,22 @@
+#%%
+#  https://school.programmers.co.kr/learn/courses/30/lessons/64065
+
+
+def solution(S):
+    S = S[2:-2].split("},{")
+    numArr = []
+    for i in range(len(S)):
+        s = S[i].split(",")
+        numArr.append(set(s))
+ 
+    numArr.sort(key=lambda x: len(x))
+ 
+    ans = set()
+    res = []
+    for a in numArr:
+        tmp = a - ans
+        res.append(list(tmp)[0])
+        ans = ans | tmp
+ 
+    res = [int(i) for i in res]
+    return res
