007645d4176a45f474b449cea471f43f26fa1f9a
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2024-03-21 13:02:53 +0900

    Create 파일명정렬.py

---
+++ Lv2/파일명정렬.py
@@ -0,0 +1,9 @@
+#%%
+# https://school.programmers.co.kr/learn/courses/30/lessons/17686
+import re
+def solution(files):
+    temp = [re.split(r"([0-9]+)", s) for s in files]
+    
+    sort = sorted(temp, key = lambda x: (x[0].lower(), int(x[1])))
+    
+    return [''.join(s) for s in sort]
