d7581be319d4306602061c32b32ea2eebf0cb967
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2024-05-09 08:47:33 +0900

    Create 우박수열 정적분.py

---
+++ Lv2/우박수열 정적분.py
@@ -0,0 +1,28 @@
+#%%
+# https://school.programmers.co.kr/learn/courses/30/lessons/134239
+
+def getUbak(k):
+    result = []
+    while k != 1:
+        result.append(k)
+        k = k / 2 if k % 2 == 0 else k * 3 + 1 
+    result.append(k)
+    return result
+ 
+def solution(k, ranges):
+    answer = []
+    ubak = getUbak(k)
+ 
+    for r in ranges:
+        total = 0
+        ubakRange = ubak[r[0] : len(ubak)+r[1]]
+        
+        if r[0] >= r[1] + len(ubak):
+            answer.append(-1)
+            continue
+            
+        for i in range(len(ubakRange) - 1):
+            total += (((ubakRange[i] + ubakRange[i+1]) * 1) / 2)
+        answer.append(total)
+        
+    return answer
