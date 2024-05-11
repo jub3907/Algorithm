9f7ffba89d7ff46956b6ce7188973e9aa18aaf43
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2024-05-12 08:58:28 +0900

    Create 덧칠하기.py

---
+++ lv1/덧칠하기.py
@@ -0,0 +1,12 @@
+#%%
+# https://school.programmers.co.kr/learn/courses/30/lessons/161989
+
+def solution(n, m, section):
+    answer = 1 # 칠하는 횟수
+    paint = section[0] # 덧칠 시작점
+    for i in range(1, len(section)):
+        if section[i] - paint >= m:
+            answer += 1
+            paint = section[i]
+            
+    return answer
