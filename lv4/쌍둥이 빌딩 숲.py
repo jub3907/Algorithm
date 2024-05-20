9bdafc40bb8d742f178d0ac63b5d2da8f51d4530
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2024-05-21 00:17:49 +0900

    Create 쌍둥이 빌딩 숲.py

---
+++ lv4/쌍둥이 빌딩 숲.py
@@ -0,0 +1,19 @@
+#%%
+#https://school.programmers.co.kr/learn/courses/30/lessons/140105
+import sys; sys.setrecursionlimit(1000000)
+cache = {(1, 1): 1}
+
+def solve(i, j):
+    if i == 0 or j == 0:
+        return 0
+    
+    if (i, j) in cache:
+        return cache[(i, j)]
+    
+    v = solve(i-1, j-1) + solve(i-1, j) * 2 * (i-1)
+    cache[(i, j)] = v % 1000000007
+    
+    return cache[(i, j)]
+    
+def solution(n, count):
+    return solve(n, count)
