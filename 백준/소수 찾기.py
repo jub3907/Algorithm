2660a2dd1c2cb84dda8e46e9fcd371a1d62bf0d9
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2024-06-13 17:55:43 +0900

    Create 소수 찾기.py

---
+++ 백준/소수 찾기.py
@@ -0,0 +1,16 @@
+#%%
+# https://www.acmicpc.net/problem/1978
+
+n = int(input())
+data = list(map(int, input().split()))
+count = 0
+
+for x in data:
+  for i in range(2, x+1):
+    if x % i == 0:
+      if x == i:
+        count += 1
+      
+      break
+
+print(count)
