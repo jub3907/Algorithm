1ec721df81922567673405129a531dcfee4594b0
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2024-06-14 17:43:31 +0900

    Create N과 M (1).py

---
+++ 백준/N과 M (1).py
@@ -0,0 +1,14 @@
+#%%
+# https://www.acmicpc.net/problem/15649
+
+import itertools
+
+n, m = map(int, input().split())
+nums = [i for i in range(1, n+1)]
+
+array = itertools.permutations(nums, m)
+
+for i in array:
+    for j in i:
+        print(j, end = ' ')
+    print()
