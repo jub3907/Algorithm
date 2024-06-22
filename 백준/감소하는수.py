0bfaf02df2a199274a2648ee54501ef73f1bde8d
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2024-06-22 19:10:05 +0900

    Create 감소하는수.py

---
+++ 백준/감소하는수.py
@@ -0,0 +1,18 @@
+#%%
+# https://www.acmicpc.net/problem/1038
+
+from itertools import combinations
+
+N = int(input())
+
+result = []
+for i in range(1, 11):
+	for j in combinations(range(10), i):
+		num = ''.join(list(map(str, reversed(list(j)))))
+		result.append(int(num))
+
+result.sort()
+if N >= len(result):
+	print(-1)
+else:
+	print(result[N])
