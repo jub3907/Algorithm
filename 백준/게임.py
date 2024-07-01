cba619c867ece5897e30833879d3bb8ce9fc2d1b
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2024-07-01 15:18:33 +0900

    Create 게임.py

---
+++ 백준/게임.py
@@ -0,0 +1,21 @@
+#%%
+# https://www.acmicpc.net/problem/1072
+
+X, Y = map(int, input().split())
+Z = (Y * 100) // X
+if Z >= 99:
+    print(-1)
+else:
+    answer = 0
+    left = 1
+    right = X
+ 
+    while left <= right:
+        mid = (left + right) // 2
+        if (Y+mid)*100 // (X+mid) <= Z:
+            left = mid+1
+        else:
+            answer = mid
+            right = mid - 1
+ 
+    print(answer)
