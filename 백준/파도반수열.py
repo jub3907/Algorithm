8c639b027c3c79e98c9d9c10c2be28c9493fa461
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2024-07-05 21:04:16 +0900

    Create 파도반수열.py

---
+++ 백준/파도반수열.py
@@ -0,0 +1,15 @@
+#%%
+#https://www.acmicpc.net/problem/9461
+
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+arr = [0, 1, 1, 1, 2, 2, 3, 4, 5, 7, 9]
+
+for i in range(11,101):
+    arr.append(arr[i-2]+arr[i-3])
+
+for _ in range(t):
+    n = int(input())
+    print(arr[n])
