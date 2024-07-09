967760ba36d61a842b9449c88fb0c808c797862d
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2024-07-10 07:36:38 +0900

    Create 배.py

---
+++ 백준/배.py
@@ -0,0 +1,27 @@
+#%%
+# https://www.acmicpc.net/problem/1092
+
+N = int(input())
+cranes = list(map(int, input().split()))
+
+M = int(input())
+boxes = list(map(int, input().split()))
+
+cranes.sort(reverse=True)
+boxes.sort(reverse=True)
+
+cnt = 0
+
+if boxes[0] > cranes[0]: cnt = -1
+else:
+    while boxes:
+        for c in cranes:
+            if boxes and c < boxes[-1]:
+                continue
+            for b in boxes:
+                if c >= b:
+                    boxes.remove(b)
+                    break
+        cnt+=1
+        
+print(cnt)
